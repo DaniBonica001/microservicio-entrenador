136f9da7164b5167c5022941f5aae8d6c8cb0c1f
G-Delgado <1006170256@u.icesi.edu.co>
2024-08-26 18:02:27 -0500

    UPDATE: Added H2 Init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO trainers (name, speciality) VALUES
+                                            ('John Doe', 'Cardio'),
+                                            ('Jane Smith', 'Strength Training'),
+                                            ('Emily Davis', 'Yoga'),
+                                            ('Michael Johnson', 'Pilates');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE trainers (
+                          trainerId BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          name VARCHAR(255),
+                          speciality VARCHAR(255)
+);
